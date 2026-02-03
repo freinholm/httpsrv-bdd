1e4846a21b7a8bf283581ffe429c4d36aa91bf0f
Freinholm <aaron.frein@presidio.edu>
2026-02-03 11:15:14 -0800

    Initial commit to stand up basic fileserver on port 8080

---
+++ go.mod
@@ -0,0 +1,3 @@
+module httpsrv-bdd
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type apiHandler struct{}
+
+func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+
+func main() {
+	const port = "8080"
+	const filepathRoot = "."
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
