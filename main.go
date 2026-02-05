5b91466e15cccce63b43441ba6ad30b8492620d8
Freinholm <aaron.frein@presidio.edu>
2026-02-05 09:54:17 -0800

    Introduce user creation & rest via API calls

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 type apiConfig struct {
 	fileserverHits 	atomic.Int32
 	db				*database.Queries
+	platform		string
 }
 
 func main() {
@@ -28,6 +29,11 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
+
   	dbURL := os.Getenv("DB_URL")
 
 	db, err := sql.Open("postgres", dbURL)
@@ -35,17 +41,21 @@ func main() {
 		log.Fatalf("error connecting to pgsql: %s", err)
 	}
 
-	dbQueries = database.New(db)
+	dbQueries := database.New(db)
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:				dbQueries,
+		platform:		platform,
 	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+
 	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
